390924165f95c9e281e73b906b5263cf8d40d4bb
Your Name <you@example.com>
2024-08-30 12:33:29 -0400

    Triangle longestLineWithin test done

--- src/test/java/edu/ithaca/dragon/shapes/TriangleTest.java
+++ src/test/java/edu/ithaca/dragon/shapes/TriangleTest.java
@@ -34,4 +34,16 @@ public class TriangleTest {
         assertEquals(.002, myTriangle.getBase());
         assertEquals(.4, myTriangle.getHeight());
     }
+
+    @Test
+    public void longestLineWithinTest(){
+        Triangle myTriangle = new Triangle(2,2);
+        assertEquals(2.83, myTriangle.longestLineWithin());
+
+        myTriangle = new Triangle(5.5, 10);
+        assertEquals(11.41, myTriangle.longestLineWithin());
+
+        myTriangle = new Triangle(.001, .2);
+        assertEquals(.2, myTriangle.longestLineWithin());
+    }
 }
