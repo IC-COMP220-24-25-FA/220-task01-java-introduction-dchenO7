55c45d285be0168756f1246f3b687bcdc6a7c57f
Your Name <you@example.com>
2024-08-30 12:19:58 -0400

    Triangle doubleSize test done

--- src/test/java/edu/ithaca/dragon/shapes/TriangleTest.java
+++ src/test/java/edu/ithaca/dragon/shapes/TriangleTest.java
@@ -16,4 +16,22 @@ public class TriangleTest {
         myTriangle = new Triangle(.001, .2);
         assertEquals(.0001, myTriangle.calcArea());
     }
+
+    @Test
+    public void doubleSize(){
+        Triangle myTriangle = new Triangle(2, 2);
+        myTriangle.doubleSize();
+        assertEquals(4, myTriangle.getBase());
+        assertEquals(4, myTriangle.getHeight());
+
+        myTriangle = new Triangle(5.5, 10);
+        myTriangle.doubleSize();
+        assertEquals(11, myTriangle.getBase());
+        assertEquals(20, myTriangle.getHeight());
+
+        myTriangle = new Triangle(.001, .2);
+        myTriangle.doubleSize();
+        assertEquals(.002, myTriangle.getBase());
+        assertEquals(.4, myTriangle.getHeight());
+    }
 }
